5a31b12ea8bece91da1b349ed80e22a897d5847f
Justin Lin <tony199462@gmail.com>
2016-11-22 18:14:00 +0800

    update JOGL Lib

--- mushroombirthday/src/backGround.java
+++ mushroombirthday/src/backGround.java
@@ -1,6 +1,6 @@
-import javax.media.opengl.GL2;
-import javax.media.opengl.glu.GLU;
-import javax.media.opengl.glu.GLUquadric;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
 
 import com.jogamp.opengl.util.texture.Texture;
 public class backGround {
--- mushroombirthday/src/cake.java
+++ mushroombirthday/src/cake.java
@@ -1,7 +1,7 @@
-import javax.media.opengl.GL;
-import javax.media.opengl.GL2;
-import javax.media.opengl.glu.GLU;
-import javax.media.opengl.glu.GLUquadric;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
 import com.jogamp.opengl.util.texture.Texture;
 
 public class cake {
